b40cfdf6fc65aca429c3427c71384b689dd8d1ea
kwasham <jwasham@ultimateimaging.com>
2016-09-15 15:12:00 -0500

    Sign In UI Custom Classes Complete

---
+++ Plink/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Plink
+//
+//  Created by Kirk Washam on 9/15/16.
+//  Copyright © 2016 StudioATX. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
+		
---
+++ Plink/FancyField.swift
@@ -0,0 +1,29 @@
+//
+//  FancyField.swift
+//  Plink
+//
+//  Created by Kirk Washam on 9/15/16.
+//  Copyright © 2016 StudioATX. All rights reserved.
+//
+
+import UIKit
+
+class FancyField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.borderColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.2).cgColor
+        layer.borderWidth = 1.0
+        layer.cornerRadius = 2.0
+        
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 10, dy: 5)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 10, dy: 5)
+    }
+
+}
